2f45d5dda7943719ce19e2a3a6521f6659d6f760
Tibi <tiurco@amberstudio.com>
2022-07-31 22:15:03 +0300

    Initial Setup

---
+++ blocks.h
@@ -0,0 +1,12 @@
+//Modify this file to change what commands output to your statusbar, and recompile using the make command.
+static const Block blocks[] = {
+	/*Icon*/	/*Command*/		/*Update Interval*/	/*Update Signal*/
+	{"", "battery",	30,		0},
+  {"", "memory", 5, 0},
+  {"", "cpu", 5, 0},
+  {"", "clock", 60, 0},
+};
+
+//sets delimeter between status commands. NULL character ('\0') means no delimeter.
+static char delim[] = " | ";
+static unsigned int delimLen = 5;
