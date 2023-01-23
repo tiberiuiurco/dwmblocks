79b8ab2cad87498362b84c85d9769d6844a03ae2
Tibi <tiberiuiurco@gmail.com>
2023-01-23 15:26:22 +0200

    Changes for main Linux PC

--- blocks.h
+++ blocks.h
@@ -1,8 +1,8 @@
 //Modify this file to change what commands output to your statusbar, and recompile using the make command.
 static const Block blocks[] = {
 	/*Icon*/	/*Command*/		/*Update Interval*/	/*Update Signal*/
-  {"", "dwm-cmus.sh", 1, 0},
-	{"", "battery",	30,		0},
+//  {"", "dwm-cmus.sh", 1, 0},
+//	{"", "battery",	30,		0},
   {"", "memory", 5, 0},
   {"", "cpu", 5, 0},
   {"", "clock", 60, 0},
